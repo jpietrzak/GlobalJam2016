e293db2fd59695df06006afaa9f02b46cff52873
Qurtaz <piwowino1994@gamil.com>
2016-01-29 19:56:19 +0100

    add plane

---
+++ Assets/Scripts/Przedmioty.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Przedmioty : MonoBehaviour {
+    private  bool zamiana=false;
+    public Material materialPoZmianaie;
+    public Renderer rend;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(zamiana==true)
+        {
+            rend.material = materialPoZmianaie;
+        }
+	
+	}
+    void zmiana()
+    {
+        zamiana = true;
+    }
+}
