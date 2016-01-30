d9d6c87538948a40712aa217aa0a028c2c5a0fa4
Maciej Kowalski <zephyel@gmail.com>
2016-01-30 14:47:25 +0100

    fix bug

--- Assets/Scripts/Nekromanta/NekromantaColider.cs
+++ Assets/Scripts/Nekromanta/NekromantaColider.cs
@@ -2,11 +2,9 @@
 using System.Collections;
 
 public class NekromantaColider : MonoBehaviour {
-    private GameObject partner;
     public float spadek = 5f;
     void Start()
     {
-        partner = GameObject.FindGameObjectWithTag("GameController");
     }
     void OnTriggerEnter(Collider collision)
     {
@@ -15,7 +13,6 @@ public class NekromantaColider : MonoBehaviour {
         if ((collision.tag == "Przedmiot") && (prz.zamiana == true))
         {
             Debug.Log("naprawa", collision.gameObject);
-            Gra gr = partner.GetComponent<Gra>();
             prz.naprawa();
             this.GetComponentInParent<Nekromanta>().obniz_stability(5);
         }
