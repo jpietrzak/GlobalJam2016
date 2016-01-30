7a52e2a0b1f28bca1aaf9185944dec970144bb52
Qurtaz <piwowino1994@gamil.com>
2016-01-30 14:37:50 +0100

    add Unicorn.prefab

--- Assets/Scripts/Wywolanie.cs
+++ Assets/Scripts/Wywolanie.cs
@@ -6,31 +6,40 @@ public class Wywolanie : MonoBehaviour {
     public GameObject wybuch;
     public GameObject wielkiDemon;
     public GameObject malyDemon;
-    public GameObject partner;
+    GameObject partner;
     float wartosc;
 	// Use this for initialization
+    void Start()
+    {
+        partner = GameObject.FindGameObjectWithTag("GameController");
+        wywolaj();
+    }
  void wywolaj()
     {
         Gra gr = partner.GetComponent<Gra>();
         wartosc = gr.stability;
         if(wartosc>70)
         {
+            Debug.Log("Wielki demon");
             Instantiate(wielkiDemon, transform.position, transform.rotation);
         }
      else
         {
             if(wartosc>40)
             {
+                Debug.Log("Mały demon");
                 Instantiate(malyDemon, transform.position, transform.rotation);
             }
             else
             {
                 if(wartosc>15)
                 {
+                    Debug.Log("Wybuch");
                     Instantiate(wybuch, transform.position, transform.rotation);
                 }
                 else
                 {
+                    Debug.Log("jednorożec");
                     Instantiate(jednorozec, transform.position, transform.rotation);
                 }
             }
