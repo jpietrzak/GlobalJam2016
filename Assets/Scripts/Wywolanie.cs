0ca24073b7aab22108ccdf78e7502e2a9d8b54d9
Qurtaz <piwowino1994@gamil.com>
2016-01-30 10:30:32 +0100

    finish Scripts/Wywolanie.cs

--- Assets/Scripts/Wywolanie.cs
+++ Assets/Scripts/Wywolanie.cs
@@ -4,33 +4,34 @@ using System.Collections;
 public class Wywolanie : MonoBehaviour {
     public GameObject jednorozec;
     public GameObject wybuch;
-    public GameObject demon;
+    public GameObject wielkiDemon;
+    public GameObject malyDemon;
     public GameObject partner;
     float wartosc;
 	// Use this for initialization
  void wywolaj()
     {
         Gra gr = partner.GetComponent<Gra>();
-        wartosc = gr.stability
+        wartosc = gr.stability;
         if(wartosc>70)
         {
-            
+            Instantiate(wielkiDemon, transform.position, transform.rotation);
         }
      else
         {
             if(wartosc>40)
             {
-
+                Instantiate(malyDemon, transform.position, transform.rotation);
             }
             else
             {
                 if(wartosc>15)
                 {
-
+                    Instantiate(wybuch, transform.position, transform.rotation);
                 }
                 else
                 {
-
+                    Instantiate(jednorozec, transform.position, transform.rotation);
                 }
             }
         }
