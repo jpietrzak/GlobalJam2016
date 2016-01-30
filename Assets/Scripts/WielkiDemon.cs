77f7f300469414b1b1ac76b96091e4fd3c497e9c
Qurtaz <piwowino1994@gamil.com>
2016-01-30 17:36:22 +0100

    add

---
+++ Assets/Scripts/WielkiDemon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WielkiDemon : MonoBehaviour {
+    private GameObject partner;
+    public GameObject pocisk;
+	// Use this for initialization
+	void Start () {
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Throw fireball");
+            Instantiate(pocisk, transform.position, transform.rotation);
+        }
+    }
+}
